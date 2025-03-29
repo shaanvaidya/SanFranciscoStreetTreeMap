7293aac27f7cc00da7fef42d05b5603ca5fbf436
shaanvaidya <spvaidya1497@gmail.com>
2025-03-29 13:18:26 -0700

    show location button on phone too

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/SanFranciscoStreetTreeMap/',
+  server: {
+    host: true, // so it binds to your local IP
+    allowedHosts: true // ⬅️ allow all hostnames like ngrok
+  }
 })
